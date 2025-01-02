5b7957802f69a89ac8dbc0ac883a2fd94c80e5f8
hector <hectorrrivera1010@gmail.com>
2025-01-02 10:56:33 -0600

    add tailwind style system

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-2xl">TEST</h1>
+      <h1 className="text-3xl">CloudWise - Cloud Data made easy</h1>
     </div>
   );
 }
