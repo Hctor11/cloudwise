12281a4d7f26acde15a6e052e09b078882b467a4
hector <hectorrrivera1010@gmail.com>
2025-01-03 01:47:52 -0600

    add design system

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl">CloudWise - Cloud Data made easy</h1>
+    <div className="center font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-3xl font-bold text-brand-100">CloudWise - Cloud Data made easy</h1>
     </div>
   );
 }
