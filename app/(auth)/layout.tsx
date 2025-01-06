30cae970f1b931ef8c6b7ee1f9c26d3b09811f33
hector <hectorrrivera1010@gmail.com>
2025-01-06 02:04:25 -0600

    add bg image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,27 +1,20 @@
 import React from "react";
-import Image from "next/image";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex min-h-screen">
-    <section className="bg-brand p-10">
-      <div>
-        <Image
-          src="/icon.svg"
-          alt="CloudWise Logo"
-          width={24}
-          height={24}
-        />
-
-        <div className="space-y-5 text-white">
-          <h1 className="h1">Manage your files</h1>
-          <p className="body-1">
-            A safety place to store all your files
-          </p>
+  return (
+    <div className=" flex min-h-screen flex-col  md:flex-row ">
+      <section className="flex-1 bg-brand p-10 flex justify-center ">
+        <div>
+          <div className="mt-5 space-y-5 text-white">
+            <h1 className="text-5xl">Manage your files effortlesly.</h1>
+          </div>
         </div>
+      </section>
+      <div className="flex-1 bg-white p-10 flex items-center justify-center">
+        {children}
       </div>
-    </section>
-    {children}
-    </div>;
+    </div>
+  );
 };
 
 export default Layout;
