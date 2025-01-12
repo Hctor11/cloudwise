98579c6b813116bea24d81b250f6fc1af8a61e27
hector <hectorrrivera1010@gmail.com>
2025-01-12 07:35:14 -0600

    add user actions function

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,11 @@
+"use server";
+
+const createAccount = async ({
+  username,
+  email,
+}: {
+  username: string;
+  email: string;
+}) => {
+    
+};
--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import { Account, Client, Databases, Storage, Avatars } from "node-appwrite";
 import { appWriteConfig } from "./config";
 import { cookies } from "next/headers";
