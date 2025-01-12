6425813737810709ea783eabf313c014dc1f7bf5
hector <hectorrrivera1010@gmail.com>
2025-01-11 18:44:51 -0600

    add user.actions.ts file

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,4 +1,4 @@
-export const appWriteCondig = {
+export const appWriteConfig = {
     endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
     projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
     databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
