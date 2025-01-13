6b6b4d1f786fcb2d7f807647c895868d3b3c1476
hector <hectorrrivera1010@gmail.com>
2025-01-12 21:01:56 -0600

    add functions in user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,39 @@
 "use server";
 
+import { createAdminClient } from "../appwrite";
+import { appWriteConfig } from "../appwrite/config";
+import { Query, ID } from "node-appwrite";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appWriteConfig.databaseId,
+    appWriteConfig.usersCollection,
+    [Query.equal("email", [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+}
+
+const sendEmailOTP = async ({email}: {email: string}) => { 
+  
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email)
+    return session.userId
+  } catch (err) {
+
+  }
+
+}
+
 const createAccount = async ({
   username,
   email,
@@ -7,5 +41,7 @@ const createAccount = async ({
   username: string;
   email: string;
 }) => {
-    
+  const existingUser = await getUserByEmail(email);
+
+  const account = await sendEmailOTP({ email })
 };
